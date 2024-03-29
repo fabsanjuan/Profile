70c5e825eee3a1ef2297976796def7f45de61ab9
Fabio Sanjuan <fabio.sanjuangomez@gmail.com>
2024-03-29 11:58:22 -0800

    Fixed the navbar for mobile devices now fully responsive. Links still not working

--- src/js/interactivity.js
+++ src/js/interactivity.js
@@ -1,11 +1,38 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const navBackground = document.querySelector('.hidden-nav-background');
+   const navBackground = document.querySelector('.hidden-nav-background'); 
     window.addEventListener('scroll', () => {
         let scrollPosition = window.scrollY;
         let windowHeight = window.innerHeight;
-
         let alpha = Math.min(scrollPosition / windowHeight, 0.4);
-        console.log(alpha);
         navBackground.style.background = `rgba(6, 9, 24, ${alpha})`;
+
+        if (scrollPosition > 0) {
+            navBackground.style.border = "solid rgb(6, 9, 24) 1px";
+        } else {
+            navBackground.style.border = "solid transparent 1px";
+        }
+
+    })
+
+    // Mobile hamburger menu.
+    const hamburger = document.getElementById('hamburger-menu');
+    const line1 = document.getElementById('hamburger-line1');
+    const line2 = document.getElementById('hamburger-line2');
+    const line3 = document.getElementById('hamburger-line3');
+    const navUL = document.querySelectorAll('.nav-left-li');
+    const menuItems = document.getElementById('menu-li-container');
+    const overlay = document.getElementById('menu-overlay');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        menuItems.classList.toggle('active');
+        overlay.classList.toggle('active');
+        line1.classList.toggle('active');
+        line2.classList.toggle('active');
+        line3.classList.toggle('active');
+        navUL.forEach(element => {
+            element.classList.toggle('active');
+            console.log(element);
+        })
     })
 })
